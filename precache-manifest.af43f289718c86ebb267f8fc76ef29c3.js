ac4eeff7e2231887ecbf1fd18ec3651e0492a508
Tanya-Goel <goeltanya01@gmail.com>
2020-07-28 16:06:40 +0530

    Updates

---
+++ precache-manifest.af43f289718c86ebb267f8fc76ef29c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c609739a672b119afb568d009bd25244",
+    "url": "/courses_react/index.html"
+  },
+  {
+    "revision": "cc954ce56ca34e0eedad",
+    "url": "/courses_react/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "1f22f203114dadfa69c3",
+    "url": "/courses_react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc954ce56ca34e0eedad",
+    "url": "/courses_react/static/js/2.dd1b299d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/courses_react/static/js/2.dd1b299d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f22f203114dadfa69c3",
+    "url": "/courses_react/static/js/main.268d75c4.chunk.js"
+  },
+  {
+    "revision": "4da04175fd6d0cb973f1",
+    "url": "/courses_react/static/js/runtime-main.abdc53a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/courses_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
